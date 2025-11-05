d48b35fb9624c149bbd3a0e53a6369c964c5eaf4
aleixanadon <aleixanadon25@gmail.com>
2025-11-05 19:34:47 +0100

    update from date to hours and wrapped text in items

--- src/components/TodoItem/TodoItem.tsx
+++ src/components/TodoItem/TodoItem.tsx
@@ -3,18 +3,18 @@ import DeleteButton from "../DeleteButton/DeleteButton";
 
 const TodoItem: React.FC<Item> = ({ id, text, date }) => {
   
-  const [day, month, year] = [
-    date.getDate(),
-    date.getMonth() + 1,
-    date.getFullYear(),
+  const [hours, minutes, seconds] = [
+    date.getHours(),
+    date.getMinutes() + 1,
+    date.getSeconds(),
   ];
 
   return (
     <li className="list-group-item d-flex justify-content-between align-items-center gap-2">
       <input type="checkbox" className="form-check-input" />
-      <div className="ms-2 me-auto fw-medium">{text}</div>
+      <div className="ms-2 text-start fw-medium text-wrap">{text}</div>
       <span className="badge text-bg-primary rounded-pill">
-        {day}/{month}/{year}
+        {hours}:{minutes}:{seconds}
       </span>
       <DeleteButton id={id} />
     </li>
